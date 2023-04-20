98ecaf8d9d86276636112e95036b7a84bc967bfa
abboldon <abboldon@yahoo.es>
2023-04-20 13:02:30 +0200

    Auto-generated commit [ci skip]

---
+++ assets/data/bonusRegistrados.ts
@@ -0,0 +1,104 @@
+import { Bonus } from '../model/entities/Bonus';
+
+export const bonusRegistrados: Bonus[] = [
+  {
+    idBonus: 1,
+    nombre: 'Fuerza Toro',
+    tipo1: 'FUE',
+    tipo2: '',
+    mod1: 4,
+    mod2: 0,
+    activo: false,
+    tipoBono: ''
+  },
+  {
+    idBonus: 2,
+    nombre: 'Gracia Felina',
+    tipo1: 'DES',
+    tipo2: '',
+    mod1: 4,
+    mod2: 0,
+    activo: false,
+    tipoBono: ''
+  },
+  {
+    idBonus: 4,
+    nombre: 'Heroismo',
+    tipo1: 'ATQ',
+    tipo2: 'SAL',
+    mod1: 2,
+    mod2: 2,
+    activo: false,
+    tipoBono: ''
+  },
+  {
+    idBonus: 5,
+    nombre: 'Heroismo Mayor',
+    tipo1: 'ATQ',
+    tipo2: 'SAL',
+    mod1: 4,
+    mod2: 4,
+    activo: false,
+    tipoBono: ''
+  },
+  {
+    idBonus: 10,
+    nombre: 'Acelerar',
+    tipo1: 'ESP',
+    tipo2: '',
+    mod1: 0,
+    mod2: 0,
+    activo: false,
+    tipoBono: ''
+  },
+  {
+    idBonus: 15,
+    nombre: 'Ataque Poderoso',
+    tipo1: 'ESP',
+    tipo2: '',
+    mod1: 0,
+    mod2: 0,
+    activo: false,
+    tipoBono: ''
+  },
+  {
+    idBonus: 20,
+    nombre: 'Arcanus(+1+3D6)',
+    tipo1: 'ESP',
+    tipo2: '',
+    mod1: 0,
+    mod2: 0,
+    activo: false,
+    tipoBono: ''
+  },
+  {
+    idBonus: 25,
+    nombre: 'Furia',
+    tipo1: 'ESP',
+    tipo2: '',
+    mod1: 0,
+    mod2: 0,
+    activo: false,
+    tipoBono: ''
+  },
+  {
+    idBonus: 20,
+    nombre: 'Arma Mágica',
+    tipo1: 'ATQ',
+    tipo2: 'DAN',
+    mod1: 1,
+    mod2: 1,
+    activo: false,
+    tipoBono: ''
+  },
+  {
+    idBonus: 20,
+    nombre: 'Arma Mágica Mayor',
+    tipo1: 'ESP',
+    tipo2: '',
+    mod1: 0,
+    mod2: 0,
+    activo: false,
+    tipoBono: ''
+  }
+];
